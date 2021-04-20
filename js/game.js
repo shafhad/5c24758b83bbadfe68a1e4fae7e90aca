381d987acc36a3eb1f485bac3e6a0143cbc7be0d
shafhad <66770462+shafhad@users.noreply.github.com>
2021-04-20 17:11:34 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -52,8 +52,8 @@ class Game{
                      x = 500-allPlayers[plr].distance;
                      y=500;
                      
-                     players[index -1].x = x;
-                     players[index - 1].y = y;
+                  //   players[index -1].x = x;
+                    // players[index - 1].y = y;
                        
                      if(index === player.index){
                          
